743bc88ef4c8d69ff16c6d2bea8b87a236736a7b
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2022-08-07 13:16:29 +0200

    file handling

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,7 +1,27 @@
 pub type Span = std::ops::Range<usize>;
+use std::sync::Arc;
+use ariadne;
 
 #[derive(Clone, Debug)]
 pub struct Spanned<T> {
+    pub source: Arc<String>,
     pub x: T, 
     pub span: Span
+}
+impl<T> ariadne::Span for Spanned<T> {
+    type SourceId = Arc<String>;
+    fn start(&self) -> usize {
+        self.span.start()
+        
+    }
+    fn end(&self) -> usize {
+        self.span.end()
+        
+    }
+    fn len(&self) -> usize {
+        ExactSizeIterator::len(&self.span)
+    }
+    fn source(&self) -> &Self::SourceId {
+        &self.source
+    }
 }
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,21 +1,14 @@
 use super::ast::*;
-use ariadne::{Color, Fmt, Label, Report, ReportKind, Source};
+use ariadne::{Color, ColorGenerator, Fmt, Label, Report, ReportKind, Source};
 
+use crate::common::*;
 use chumsky::prelude::*;
 use chumsky::Stream;
 use core::fmt;
 use std::fs::*;
 use std::io;
 use std::sync::Arc;
-//
 
-pub fn merge_span(s1: Span, s2: Span) -> Span {
-    let (low1, high1) = (s1.start(), s1.end());
-    let (low2, high2) = (s2.start(), s2.end());
-    let low = std::cmp::max(low1, low2);
-    let high = std::cmp::max(high1, high2);
-    low..high
-}
 #[derive(Debug)]
 pub struct ParseError {}
 
@@ -135,45 +128,66 @@ fn lexer() -> impl Parser<char, Vec<(Token, Span)>, Error = Simple<char>> {
         .repeated()
 }
 
-fn parser() -> impl Parser<Token, Spanned<LispVal>, Error = Simple<Token>> + Clone {
-    recursive(|expr| {
+fn parser(
+    origin_src: Arc<String>,
+) -> impl Parser<Token, Spanned<LispVal>, Error = Simple<Token>> + Clone {
+    recursive(move |expr| {
         let ident = select! { Token::Ident(ident) => ident.clone() }.labelled("identifier");
         let params = ident.repeated();
 
         let num = select! { Token::Num(v) => v.clone() }.labelled("number");
-        let num = num.map_with_span(|v, span: Span| Spanned {
+
+        let source = origin_src.clone();
+        let num = num.map_with_span(move |v, span: Span| Spanned {
+            source: source.clone(),
             x: Arc::new(LispValX::Int(v)),
             span,
         });
 
+        let source = origin_src.clone();
         let string = select! { Token::Str(s) => s.clone() }.labelled("string");
-        let string = string.map_with_span(|v, span| Spanned {
+        let string = string.map_with_span(move |v, span| Spanned {
+            source: source.clone(),
             x: Arc::new(LispValX::String(v)),
             span,
         });
 
+        let source = origin_src.clone();
         let boolean = select! { Token::Bool(b) => b.clone() }.labelled("bool");
-        let boolean = boolean.map_with_span(|b, span| Spanned {
+        let boolean = boolean.map_with_span(move |b, span| Spanned {
+            source: source.clone(),
             x: Arc::new(LispValX::Bool(b)),
             span,
         });
 
+        let source = origin_src.clone();
         let atom = select! { Token::Ident(ident) => ident.clone() }.labelled("atom");
-        let atom = atom.map_with_span(|s, span| Spanned {
+        let atom = atom.map_with_span(move |s, span| Spanned {
+            source: source.clone(),
             x: Arc::new(LispValX::Atom(s)),
             span,
         });
 
+        let source = origin_src.clone();
+        let source2 = origin_src.clone();
+
         let arith_ops = (just(Token::Neg)
             .or(just(Token::Add))
             .or(just(Token::Div))
-            .or(just(Token::Mult)))
-        .map_with_span(|tok, span: Span| Spanned {
+            .or(just(Token::Mult))
+            .or(just(Token::Ge))
+            .or(just(Token::Geq))
+            .or(just(Token::Le))
+            .or(just(Token::Leq))
+            .or(just(Token::Eq)))
+        .map_with_span(move |tok, span: Span| Spanned {
+            source: source.clone(),
             x: Arc::new(LispValX::Atom(Arc::new(tok.to_string()))),
             span,
         })
         .then(expr.clone().repeated())
-        .map_with_span(|(op, mut args), span: Span| Spanned {
+        .map_with_span(move |(op, mut args), span: Span| Spanned {
+            source: source2.clone(),
             x: Arc::new(LispValX::List(Arc::new({
                 let mut v = Vec::new();
                 v.push(op);
@@ -183,16 +197,20 @@ fn parser() -> impl Parser<Token, Spanned<LispVal>, Error = Simple<Token>> + Clo
             span,
         });
 
+        let source = origin_src.clone();
+
         let lvals = just(Token::LParen)
             .ignore_then(expr.repeated())
             .then_ignore(just(Token::RParen))
-            .map_with_span(|lvals, span| Spanned {
+            .map_with_span(move |lvals, span| Spanned {
+                source: source.clone(),
                 x: Arc::new(LispValX::List(Arc::new(lvals))),
                 span,
             });
 
         let body = lvals.clone().repeated();
 
+        let source = origin_src.clone();
         let func_name_with_params = just(Token::LParen)
             .ignore_then(just(Token::Define))
             .ignore_then(just(Token::LParen))
@@ -201,9 +219,14 @@ fn parser() -> impl Parser<Token, Spanned<LispVal>, Error = Simple<Token>> + Clo
             .then_ignore(just(Token::RParen))
             .then(body)
             .then_ignore(just(Token::RParen))
-            .map_with_span(|((ident, params), body), span: Span| Spanned{
-                x: Arc::new(LispValX::Function{name: ident, params, body: Arc::new(body)}), 
-                span
+            .map_with_span(move |((ident, params), body), span: Span| Spanned {
+                source: source.clone(),
+                x: Arc::new(LispValX::Function {
+                    name: ident,
+                    params,
+                    body: Arc::new(body),
+                }),
+                span,
             });
 
         let out = func_name_with_params
@@ -217,32 +240,37 @@ fn parser() -> impl Parser<Token, Spanned<LispVal>, Error = Simple<Token>> + Clo
     })
 }
 
-fn module_parser() -> impl Parser<Token, LispModule, Error=Simple<Token>> + Clone {
-    parser().repeated().then_ignore(end()).map(|fns| Arc::new(LispModuleX {functions: Arc::new(fns)}))
-
+fn module_parser(
+    source: Arc<String>,
+) -> impl Parser<Token, LispModule, Error = Simple<Token>> + Clone {
+    parser(source).repeated().then_ignore(end()).map(|fns| {
+        Arc::new(LispModuleX {
+            functions: Arc::new(fns),
+        })
+    })
 }
 
 pub fn parse_file(filename: &str) -> Result<(), ParseError> {
     let src = read_to_string(filename)?;
 
-    let len = src.chars().count();
     let (tokens, errs) = lexer().parse_recovery(src.as_str());
 
     let parse_errs = if let Some(tokens) = tokens {
         let len = src.chars().count();
-        let (ast, parse_errs) =
-            module_parser().parse_recovery(Stream::from_iter(len..len + 1, tokens.into_iter()));
+        let (ast, parse_errs) = module_parser(Arc::new(filename.to_string()))
+            .parse_recovery(Stream::from_iter(len..len + 1, tokens.into_iter()));
 
-        println!("{:?}", ast);
+        println!("{:#?}", ast);
         parse_errs
     } else {
         Vec::new()
     };
+
     errs.into_iter()
         .map(|e| e.map(|c| c.to_string()))
         .chain(parse_errs.into_iter().map(|e| e.map(|tok| tok.to_string())))
         .for_each(|e| {
-            let report = Report::build(ReportKind::Error, (), e.span().start);
+            let report = Report::build(ReportKind::Error, "", 10);
 
             let report = match e.reason() {
                 chumsky::error::SimpleReason::Unclosed { span, delimiter } => report
@@ -251,7 +279,7 @@ pub fn parse_file(filename: &str) -> Result<(), ParseError> {
                         delimiter.fg(Color::Yellow)
                     ))
                     .with_label(
-                        Label::new(span.clone())
+                        Label::new((filename,span.clone()))
                             .with_message(format!(
                                 "Unclosed delimiter {}",
                                 delimiter.fg(Color::Yellow)
@@ -259,7 +287,7 @@ pub fn parse_file(filename: &str) -> Result<(), ParseError> {
                             .with_color(Color::Yellow),
                     )
                     .with_label(
-                        Label::new(e.span())
+                        Label::new((filename,e.span()))
                             .with_message(format!(
                                 "Must be closed before this {}",
                                 e.found()
@@ -289,7 +317,7 @@ pub fn parse_file(filename: &str) -> Result<(), ParseError> {
                         }
                     ))
                     .with_label(
-                        Label::new(e.span())
+                        Label::new((filename, e.span()))
                             .with_message(format!(
                                 "Unexpected token {}",
                                 e.found()
@@ -299,13 +327,15 @@ pub fn parse_file(filename: &str) -> Result<(), ParseError> {
                             .with_color(Color::Red),
                     ),
                 chumsky::error::SimpleReason::Custom(msg) => report.with_message(msg).with_label(
-                    Label::new(e.span())
+                    Label::new((filename, e.span()))
                         .with_message(format!("{}", msg.fg(Color::Red)))
                         .with_color(Color::Red),
                 ),
             };
 
-            report.finish().print(Source::from(&src)).unwrap();
+            report.finish()
+                .print((filename, Source::from(&src)))
+                .unwrap();
         });
 
     todo!();
